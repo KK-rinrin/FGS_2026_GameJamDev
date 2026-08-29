3888c89e619406d0ead9103839106d382a1bf837
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-08-30 04:52:02 +0900

    Revert "Merge pull request #24 from shinonome-MiDUki/merging2"
    This reverts commit 3b5d2ed47d31ee7650210893bd621b2c72a618ec, reversing
    changes made to c1a339518f934dcb8a855516c51ece96760c4a05.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,12 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : SoundManagerScript
+public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-<<<<<<< HEAD
-    [SerializeField] private AudioSource main_audiosource;
-=======
     private Animator animator;
 
->>>>>>> upstream/main
     private SpriteRenderer sprite;
     [Header("HP管理スクリプト")]
     [SerializeField] private PlayerHpScript playerHpScript;
@@ -60,7 +56,6 @@ public class Player : SoundManagerScript
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        PlayMainBgm(main_audiosource);
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
@@ -79,16 +74,9 @@ public class Player : SoundManagerScript
         state = PlayerState.Running;
     }
 
-
     // Update is called once per frame
     void Update()
     {
-        #if DEBUG
-        if (Input.GetKeyDown(KeyCode.Return)){
-            OnJumpSE();
-        }
-        #endif
-
         switch(state)
         {
             case PlayerState.None:
@@ -121,7 +109,6 @@ public class Player : SoundManagerScript
 
         if (!isJumping && state != PlayerState.Inverting && Keyboard.current.pKey.wasPressedThisFrame)
         {
-            OnJumpSE();
             // ジャンプの力を重力の符号によって向きを変えながら与える
             rb.AddForce(new Vector2(0.0f, kJumpPower * Mathf.Sign(rb.gravityScale)));
 
@@ -135,7 +122,7 @@ public class Player : SoundManagerScript
             if (Keyboard.current.spaceKey.wasPressedThisFrame)
             {
                 state = PlayerState.Inverting;
-                OnReverseSE();
+
                 rb.gravityScale *= -1.0f;
 
                 sprite.flipY = !sprite.flipY;
@@ -202,7 +189,6 @@ public class Player : SoundManagerScript
         {
             if(state != PlayerState.Damage)
             {
-                OnCollisionSE();
                 state = PlayerState.Damage;
                 animator.Play("player_damage");
                 timeCount = 0.0f;
