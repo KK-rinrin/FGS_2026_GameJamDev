b4cc334ed6c954bf73dad1eb09383b89693d2690
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-08-30 13:52:55 +0900

    ジャンプのキーをzキーに変更

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -122,7 +122,7 @@ public class Player : SoundManagerScript
     {
         currentInvertTime += Time.deltaTime;
 
-        if (!isJumping && state != PlayerState.Inverting && Keyboard.current.pKey.wasPressedThisFrame)
+        if (!isJumping && state != PlayerState.Inverting && Keyboard.current.zKey.wasPressedThisFrame)
         {
             OnJumpSE();
             // ƒWƒƒƒ“ƒv‚Ì—Í‚ğd—Í‚Ì•„†‚É‚æ‚Á‚ÄŒü‚«‚ğ•Ï‚¦‚È‚ª‚ç—^‚¦‚é
