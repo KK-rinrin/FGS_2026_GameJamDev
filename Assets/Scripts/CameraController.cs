5d58d26601dd19b21d4a8e7d0523891e3f888f4f
KaminagaAyumu <kaminagajobsearch@gmail.com>
2026-08-30 10:00:25 +0900

    カメラを回転させる処理を実装

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private CinemachineCamera cinemachineCamera;
+
+    [Header("ÉJÉÅÉâÇÃìÆÇ≠éûä‘")]
+    [SerializeField] private float kCameraMoveTime = 0.1f;
+
+    private float currentRot;
+    private float targetRot;
+
+    private float time = 0.0f;
+    private bool isInversing = false;
+    public void Inverse()
+    {
+        currentRot = cinemachineCamera.transform.localEulerAngles.z;
+        targetRot += 180.0f;
+
+        time = 0.0f;
+
+        isInversing = true;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cinemachineCamera = GetComponent<CinemachineCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isInversing)
+        {
+            return;
+        }
+
+        time += Time.deltaTime;
+
+        float rate = Mathf.Clamp01(time / kCameraMoveTime);
+
+        rate = Mathf.SmoothStep(0.0f, 1.0f, rate);
+
+        float rotZ = Mathf.LerpAngle(currentRot, targetRot, rate);
+
+        cinemachineCamera.transform.localEulerAngles = new Vector3(0.0f, 0.0f,rotZ);
+
+        if (time >= kCameraMoveTime)
+        {
+            isInversing = false;
+            currentRot = targetRot;
+        }
+    }
+}
