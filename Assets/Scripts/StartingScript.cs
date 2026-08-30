40d9fc3b9224f33613828b8f322fe1b16d2d1c72
shinonome-MiDUki <shiinaayame3@gmail.com>
2026-08-30 09:16:54 +0900

    revert to right files

--- Assets/Scripts/StartingScript.cs
+++ Assets/Scripts/StartingScript.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
 using UnityEditor;
+using System;
+using UnityEngine.UI;
 
-public class StartingScript : MonoBehaviour
+public class StartingScript : SoundManagerScript
 {
+    [SerializeField] private AudioSource starting_audiosource;
+    [SerializeField] private Slider audio_volume_slider;
+
     void Start()
     {
-        
+        PlayStartingBgm(starting_audiosource);
+        // starting_audiosource.clip = starting_bgm;
+        // starting_audiosource.Play();
     }
 
     public void StartGameBTN()
     {
+        OnClickSE();
+        starting_audiosource.Stop();
         UnityEngine.SceneManagement.SceneManager.LoadScene("KaminagaScene");
     }
 
     public void QuitGameBTN()
     {
+        OnClickSE();
+        starting_audiosource.Stop();
         #if UNITY_EDITOR
         EditorApplication.isPlaying = false;
         #else
@@ -24,6 +35,13 @@ public class StartingScript : MonoBehaviour
 
     public void RetryGameBTN()
     {
+        OnClickSE();
+        starting_audiosource.Stop();
         Initiate.Fade("TitleScene", Color.black, 1.0f);
+    }
+
+    public void AudioVolumeSLIDER()
+    {
+        starting_audiosource.volume = audio_volume_slider.value;
     }
 }
